4c2e54f3318e8c8a66ab0d8983879336965d6bd7
yejoon Cho <uactye@LAPTOP-QEMTIR7D.localdomain>
2021-11-05 22:50:06 +0900

    Design : set up Apptheme
    앱 테마의 light mode 와 dark mode 각각 설정함.

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:counter_app/core/themes/app_theme.dart';
+import 'package:counter_app/presentation/screens/counter_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Counter App',
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.light,
+      debugShowCheckedModeBanner: false,
+      home: CounterScreen(),
+    );
+  }
+}
---
+++ lib/presentation/screens/counter_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class CounterScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Counter Screen'),
+      ),
+      body: Center(
+        child: Text(
+          'Welcome to Counter App',
+          style: TextStyle(fontSize: 28),
+        ),
+      ),
+    );
+  }
+}
