bc300908cdf31a4a8ac38cba17c27530c451370b
yejoon Cho <uactye@LAPTOP-QEMTIR7D.localdomain>
2021-11-07 08:58:39 +0900

    FEAT: add animation to the slider bar
    슬라이더바에 좌우로 슬라이드를 할 수 있는 에니메이션을 추가하였습니다. 기본적으로 좌우로 슬라이드 할수있고 한계점에 도달하면 버튼이 흔들리면서 중앙으로 초기화됩니다.

--- lib/presentation/widgets/counter_slide.dart
+++ lib/presentation/widgets/counter_slide.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/physics.dart';
 
 class CounterSlide extends StatefulWidget {
   @override
@@ -7,6 +8,34 @@ class CounterSlide extends StatefulWidget {
 
 class _CounterSlideState extends State<CounterSlide>
     with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _animation;
+  double? _startAnimationPosX;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller =
+        AnimationController(vsync: this, lowerBound: -0.5, upperBound: 0.5);
+    _controller.value = 0;
+    _controller.addListener(() {});
+    _animation = Tween<Offset>(begin: Offset(0.0, 0.0), end: Offset(1.5, 0.0))
+        .animate(_controller);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  void didUpdateWidget(covariant CounterSlide oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    _animation = Tween<Offset>(begin: Offset(0.0, 0.0), end: Offset(1.5, 0.0))
+        .animate(_controller);
+  }
+
   @override
   Widget build(BuildContext context) {
     return FittedBox(
@@ -39,18 +68,28 @@ class _CounterSlideState extends State<CounterSlide>
                   color: Theme.of(context).iconTheme.color!.withOpacity(0.7),
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Material(
-                  color: Theme.of(context).accentColor,
-                  shape: const CircleBorder(),
-                  elevation: 5.0,
-                  child: Center(
-                    child: Icon(
-                      Icons.trip_origin,
-                      size: 40,
-                      color:
-                          Theme.of(context).iconTheme.color!.withOpacity(0.6),
+              GestureDetector(
+                onHorizontalDragStart: _onPanStart,
+                onHorizontalDragUpdate: _onPanUpdate,
+                onHorizontalDragEnd: _onPanEnd,
+                child: SlideTransition(
+                  position: _animation,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Material(
+                      color: Theme.of(context).accentColor,
+                      shape: const CircleBorder(),
+                      elevation: 5.0,
+                      child: Center(
+                        child: Icon(
+                          Icons.trip_origin,
+                          size: 40,
+                          color: Theme.of(context)
+                              .iconTheme
+                              .color!
+                              .withOpacity(0.6),
+                        ),
+                      ),
                     ),
                   ),
                 ),
@@ -60,5 +99,33 @@ class _CounterSlideState extends State<CounterSlide>
         ),
       ),
     );
+  }
+
+  double offsetFromGlobalPos(Offset globalPosition) {
+    RenderBox box = context.findRenderObject() as RenderBox;
+    Offset local = box.globalToLocal(globalPosition);
+    _startAnimationPosX = ((local.dx * 0.75) / box.size.width) - 0.4;
+    return ((local.dx * 0.75) / box.size.width) - 0.4;
+  }
+
+  void _onPanStart(DragStartDetails details) {
+    _controller.stop();
+    _controller.value = offsetFromGlobalPos(details.globalPosition);
+  }
+
+  void _onPanUpdate(DragUpdateDetails details) {
+    _controller.value = offsetFromGlobalPos(details.globalPosition);
+  }
+
+  void _onPanEnd(DragEndDetails details) {
+    _controller.stop();
+    final SpringDescription _kDefaultSpring =
+        new SpringDescription.withDampingRatio(
+      mass: 0.9,
+      stiffness: 250.0,
+      ratio: 0.6,
+    );
+    _controller.animateWith(
+        SpringSimulation(_kDefaultSpring, _startAnimationPosX!, 0.0, 0.0));
   }
 }
