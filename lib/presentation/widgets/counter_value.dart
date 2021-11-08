5200f0f164ebb2c7057aba9c6f31c5508d2b0e8a
yejoon Cho <uactye@LAPTOP-QEMTIR7D.localdomain>
2021-11-08 13:58:50 +0900

    Design: add an animation to count value
    counter value에 애니메이션을 추가했습니다.

--- lib/presentation/widgets/counter_value.dart
+++ lib/presentation/widgets/counter_value.dart
@@ -1,19 +1,39 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:counter_app/logic/counter_cubit.dart';
 import 'package:counter_app/logic/counter_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class CounterValue extends StatelessWidget {
+class CounterValue extends StatefulWidget {
+  @override
+  _CounterValueState createState() => _CounterValueState();
+}
+
+class _CounterValueState extends State<CounterValue> {
+  AnimationController? _animationController;
+
+  @override
+  void dispose() {
+    _animationController?.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<CounterCubit, CounterState>(
       builder: (context, state) {
-        return Text(
-          '${state.counterValue}',
-          style: Theme.of(context).textTheme.headline1!.copyWith(
-                fontWeight: FontWeight.bold,
-                fontSize: 128,
-              ),
+        _animationController?.forward(from: 0.0);
+        return ZoomIn(
+          duration: Duration(milliseconds: 400),
+          manualTrigger: true,
+          controller: (controller) => _animationController = controller,
+          child: Text(
+            '${state.counterValue}',
+            style: Theme.of(context).textTheme.headline1!.copyWith(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 128,
+                ),
+          ),
         );
       },
     );
